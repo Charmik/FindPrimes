912a5f5019d7310f6097eefd56b3142562da92ac
Charmik <charmik1994@gmail.com>
2015-03-19 22:44:50 +0300

    from master1

--- src/main/ru/ifmo/ctdev/lovkov/Main.java
+++ src/main/ru/ifmo/ctdev/lovkov/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int start = 20;
+        int start = 13;
         int finish = 10000;
 
         FindPrimers findPrimers = new FindPrimers();
